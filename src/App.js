bdeebf5486ecf17b64116d9a4ef708f52468a3c6
Sergey Solovyev <solovyov.sa@gmail.com>
2020-05-16 18:12:44 +0300

    Added type

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
+import { Redirect } from 'react-router';
 
 import './App.css';
 
@@ -45,15 +46,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}></Route>
           <Route path='/shop' component={ShopPage}></Route>
-          <Route path='/signin' component={SignInAndSignUpPage}></Route>
+          <Route 
+            exact 
+            path='/signin' 
+            render={() => 
+              this.props.currentUser ? (<Redirect to='/' />) 
+              : (<SignInAndSignUpPage/>)
+            }
+          ></Route>
         </Switch>    
       </div>
     );
   }  
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
